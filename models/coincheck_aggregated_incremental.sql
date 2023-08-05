48e17899d301c638bb16d96508196a00d383efc3
roshangandhi@hotmail.co.uk <roshangandhi@hotmail.co.uk>
2023-08-05 07:41:04 +0000

    Explicitly defining date partitions to replace

--- models/coincheck_aggregated_incremental.sql
+++ models/coincheck_aggregated_incremental.sql
@@ -1,12 +1,17 @@
+{% set partitions_to_replace = [
+    "current_date()",
+    "date_sub(current_date, interval 1 day)",
+] %}
 {{
     config(
         materialized="incremental",
         partition_by={"field": "transaction_date", "data_type": "date"},
         incremental_strategy="insert_overwrite",
+        partitions=partitions_to_replace,
     )
 }}
 select *
-from {{ref('coincheck_aggregated')}}
+from {{ ref("coincheck_aggregated") }}
 {% if is_incremental() %}
-    where transaction_date >= date_sub(date(_dbt_max_partition), interval 1 day)
+    where transaction_date in ({{ partitions_to_replace | join(",") }})
 {% endif %}
